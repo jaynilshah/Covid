27784c29713bb9ef773eb5cfe2c1546d2eb6cb83
Jaynil Shah <jaynil.shah84@gmail.com>
2020-06-14 18:03:45 -0400

    doctor login logout working

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,16 +1,27 @@
 const {User} = require('./../models/user');
+const {Doctor} = require('./../models/doctor');
 
 
 var authenticate = (req,res,next)=>{
     var token = req.header('x-auth');
     User.findByToken(token).then((user)=>{
         if(!user){
-            // res.status(401).send('user not found'            
+            // res.status(401).send('user not found'
+            Doctor.findByToken(token).then((doc)=>{
+                if(!doc){
+                   return Promise.reject(); 
+                }
+                req.user = doc;
+                req.token = token;
+                next();
+            })            
             return Promise.reject();
         }
-        req.user = user;
-        req.token= token;
-        next();
+        else{
+            req.user = user;
+            req.token= token;
+            next();
+        }
     }).catch((e)=>{
         res.status(401).send();
     });
--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const cors = require('cors');
 const port = 3000;
 var app = express();
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.listen(port,()=>{
@@ -48,12 +53,23 @@ app.post('/doctors',(req,res)=>{
 
 
 //login
-app.post('/users/login',(req,res)=>{
+app.post('/usersLogin',(req,res)=>{
     var body = _.pick(req.body,['email','password']);
     
     User.findByCredentials(body.email,body.password).then((user)=>{
         return user.generateAuthToken().then((token)=>{
             res.header('x-auth',token).send(user);
+        })
+    })
+    .catch((e)=>{
+        res.status(400).send(e);
+    });    
+})
+app.post('/doctorsLogin',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    Doctor.findByCredentials(body.email,body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.send({token,email:user.email});
         })
     })
     .catch((e)=>{
@@ -70,6 +86,12 @@ app.get('/temporary',(req,res)=>{
 })
 
 //logout
+app.delete('/users/me/token',authenticate,(req,res)=>{
+    req.user.removeToken(req.token).then(()=>{
+        res.status(200).send();
+    }).catch((e)=>res.status(400));
+})
+
 app.delete('/users/me/token',authenticate,(req,res)=>{
     req.user.removeToken(req.token).then(()=>{
         res.status(200).send();
